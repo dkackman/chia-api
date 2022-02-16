4c3c73409bdd7f07922112221e6fdf67eedbeb11
Don Kackman <dkackman@gmail.com>
2022-02-16 16:55:45 -0600

    make index.html static

--- build-site.ps1
+++ build-site.ps1
@@ -23,25 +23,3 @@ foreach ($file in $files)
     $shortFile = [IO.Path]::GetFileNameWithoutExtension($file)
     Copy-Item .\build\tmp\$file\index.html -Destination .\build\site\$shortFile.html
 }
-
-New-Item .\build\site\index.html -ItemType File
-$html = @"
-<html><head>
-<meta http-equiv='X-UA-Compatible' content='IE=edge' />
-<title>Chia RPC</title>
-<meta name='viewport' content='width=device-width, initial-scale=1.0'>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-<meta charset='UTF-8' />
-</head><body><div><ul>
-"@
-
-foreach ($file in $files)
-{
-    $shortFile = [IO.Path]::GetFileNameWithoutExtension($file)
-    $a = "<li><a href='./$shortFile.html'>$shortfile</a></li>"
-    $html += $a
-}
-
-$html += "</ul></div></body></html>"
-
-Set-Content .\build\site\index.html $html
