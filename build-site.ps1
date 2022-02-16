977c353827da4af19178fa79fb0ceda188068487
Don Kackman <dkackman@gmail.com>
2022-02-16 17:09:47 -0600

    v2

--- build-site.ps1
+++ build-site.ps1
@@ -1,19 +1,15 @@
 Remove-Item ./build -Force -Recurse
+
 mkdir ./build
 
 # first mash all the disparate files into 1 file per endpoint
 $files = Get-ChildItem .\src\*.yaml -name
 
-# foreach ($file in $files)
-# {
-#    swagger-cli bundle .\src\$file -t yaml -o .\build\$file
-# }
-
 mkdir ./build/tmp
 
 foreach ($file in $files)
 {
-    java -jar ./tools/swagger-codegen-cli.jar generate -l html2 -i ./src/wallet.yaml -o .\build\tmp\$file
+    java -jar ./tools/swagger-codegen-cli.jar generate -l html2 -i ./src/$file -o .\build\tmp\$file
 }
 
 mkdir ./build/site
