74100c9932e1c8f016878f90b0ee4ae3678179f2
Don Kackman <dkackman@gmail.com>
2022-02-24 09:43:29 -0600

    show base websocket command for each RPC method

--- build1.ps1
+++ build1.ps1
@@ -8,10 +8,10 @@ mkdir ./build/site
 
 # foreach ($file in $files)
 
-$file = "wallet.yaml"
+$file = "daemon.yaml"
 $shortFile = [IO.Path]::GetFileNameWithoutExtension($file)
 java -jar ./tools/swagger-codegen-cli.jar generate -l html2 -i ./src/$file -o .\build\tmp\$file `
-    -t tools/templates/htmlDocs2 --additional-properties endpoint=$shortFile
+    -t tools/templates/htmlDocs2 --additional-properties endpoint=$shortFile websocket=true
 Copy-Item .\build\tmp\$file\index.html -Destination .\build\site\$shortFile.html
 
 Copy-Item -Path ./build/site/$shortFile.html -Destination ./docs/static/ -Force
