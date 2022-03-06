ea80806475d91217c5553b087b8a17730c392ad0
Don Kackman <dkackman@gmail.com>
2022-03-06 11:04:47 -0600

    lint

--- build1.ps1
+++ build1.ps1
@@ -4,14 +4,10 @@ mkdir ./build
 mkdir ./build/tmp
 mkdir ./build/site
 
-#v$files = Get-ChildItem .\src\*.yaml -name
-
-# foreach ($file in $files)
-
 $file = "daemon.yaml"
 $shortFile = [IO.Path]::GetFileNameWithoutExtension($file)
-java -jar ./tools/swagger-codegen-cli.jar generate -l html2 -i ./src/$file -o .\build\tmp\$file `
+java -jar ./tools/swagger-codegen-cli.jar generate -l html2 -i ./src/$file -o ./build/tmp/$file `
     -t tools/templates/htmlDocs2 --additional-properties endpoint=$shortFile websocket=true
-Copy-Item .\build\tmp\$file\index.html -Destination .\build\site\$shortFile.html
 
+Copy-Item ./build/tmp/$file/index.html -Destination ./build/site/$shortFile.html
 Copy-Item -Path ./build/site/$shortFile.html -Destination ./docs/static/ -Force
