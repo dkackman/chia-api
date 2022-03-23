eb830a815055847ea1402b266ecb75c986e43123
Don Kackman <dkackman@gmail.com>
2022-03-23 11:12:15 -0500

    extra copy not needed

--- build1.ps1
+++ build1.ps1
@@ -9,15 +9,14 @@ $shortFile = [IO.Path]::GetFileNameWithoutExtension($file)
 
 # this does swagger-codegen
 if ($shortFile -eq "daemon") { # need to let the custom codegen template know to use websocket for deamon
-    java -jar ./tools/swagger-codegen-cli.jar generate -l html2 -i ./src/$file -o .\build\tmp\$file `
+    java -jar ./tools/swagger-codegen-cli.jar generate -l html2 -i ./src/$file -o ./build/tmp/$file `
         -t tools/templates/htmlDocs2 --additional-properties endpoint=$shortFile websocket=true
 }
 else {
-    java -jar ./tools/swagger-codegen-cli.jar generate -l html2 -i ./src/$file -o .\build\tmp\$file `
+    java -jar ./tools/swagger-codegen-cli.jar generate -l html2 -i ./src/$file -o ./build/tmp/$file `
         -t tools/templates/htmlDocs2 --additional-properties endpoint=$shortFile
 }
 Copy-Item ./build/tmp/$file/index.html -Destination ./build/site/$shortFile.html
-Copy-Item -Path ./build/site/$shortFile.html -Destination ./docs/static/ -Force
 
 # this does redocly
 npx redoc-cli bundle src/$file --output build/redoc/$shortFile.html --options.theme.colors.primary.main=green
