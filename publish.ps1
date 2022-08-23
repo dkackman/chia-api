ed5dc28324803ea3816aa3535ac65e4aa2e28cb3
Don Kackman <dkackman@gmail.com>
2022-08-23 18:44:08 -0500

    1.5.1

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,2 @@
-Copy-Item -Path ./build/site/* -Destination ./docs/static/ -Force
+#Copy-Item -Path ./build/site/* -Destination ./docs/static/ -Force
 Copy-Item -Path ./build/redoc/* -Destination ./docs/redoc/ -Force
